fc9431ed0dc086980cb2a4591262c1a885635874
trieuvanthao <trieuvanthao95@gmail.com>
2022-06-07 09:48:38 +0700

    fix benh an noi khoa

--- src/app/admin/HoiBenh/HoiBenh_NoiKhoa.component.ts
+++ src/app/admin/HoiBenh/HoiBenh_NoiKhoa.component.ts
@@ -62,5 +62,20 @@ export class HoiBenh_NoiKhoa implements OnInit {
         });
     }
 
+    appendVal(obj: any, key: any, value: any) {
+        if(this._isNullOrEmpty(obj)){
+            obj = {}
+        }
+        obj[key] = value;
+        return obj;
+    }
+
+    _isNullOrEmpty(value: any) {
+        if (value == null || value == undefined || value == '') {
+            return true;
+        }
+        return false;
+    }
+
 
 }
